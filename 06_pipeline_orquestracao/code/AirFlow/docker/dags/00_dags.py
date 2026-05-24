f5f757e3804b2e42c47c17e53a1fc36d0d9fab24
LuizHenriqueEgito <luiz_henriquesegito@yhaoo.com.br>
2026-05-23 21:49:25 -0300

    m

---
+++ 06_pipeline_orquestracao/code/AirFlow/docker/dags/00_dags.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+
+def task_hello():
+    print("Airflow está funcionando corretamente!")
+
+
+def task_second():
+    print("Segunda task executada com sucesso!")
+
+
+with DAG(
+    dag_id="teste_simples",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+    tags=["teste"],
+) as dag:
+
+    t1 = PythonOperator(
+        task_id="hello_task",
+        python_callable=task_hello,
+    )
+
+    t2 = PythonOperator(
+        task_id="second_task",
+        python_callable=task_second,
+    )
+
+    t1 >> t2
